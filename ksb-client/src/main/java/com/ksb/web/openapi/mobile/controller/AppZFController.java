d994ce138642b6c773af8db06e5ee4b2e2816ccd
houshipeng <hsp2004@126.com>
2015-10-10 13:42:33 +0800

    增加ping++支付相关

--- ksb-client/src/main/java/com/ksb/web/openapi/mobile/controller/AppZFController.java
+++ ksb-client/src/main/java/com/ksb/web/openapi/mobile/controller/AppZFController.java
@@ -1,35 +1,32 @@
 package com.ksb.web.openapi.mobile.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ksb.openapi.entity.ResultEntity;
+import com.ksb.openapi.error.BaseSupportException;
 import com.ksb.openapi.mobile.service.ChargeService;
-import com.pingplusplus.Pingpp;
+import com.pingplusplus.model.Charge;
 
 @Controller
 @RequestMapping("/mobile/pay")
 public class AppZFController {
 
-	@Value("#{sys['pingxx_app_id']}")
-	public String PINGXX_API_ID = null;
-	
-	public String PINGXX_API_KEY = null;
-	
-	public ChargeService chargeService;
-	
-	/*初始化设置ping++ app key*/
-	public AppZFController(){
-		Pingpp.apiKey = PINGXX_API_KEY;
-	}
-	
-	@Value("#{sys['pingxx_app_key']}")
-	public void setPingxxApiKey(String appKey){
-		this.PINGXX_API_KEY = appKey;
-	}
+	@Autowired
+	ChargeService chargeService;
 	
 	/**
 	 * 商家充值
@@ -39,14 +36,115 @@ public class AppZFController {
 	 */
 	@RequestMapping(value="/sp_recharge",method=RequestMethod.POST)
 	public @ResponseBody
-	       ResultEntity shipperRecharge(String channel_name,String amount,String sp_id){
+	       Object shipperRecharge(HttpServletRequest req){
 		
-		chargeService.shipperRecharge(channel_name, amount, sp_id, PINGXX_API_ID);
+		String postData = null;
+		try{
+			postData = getPostData(req);
+		}catch(Exception e){
+			return getErrorEntity(e.getMessage());
+		}
+		if(postData==null){
+			return errorPayInfo("未提供任何参数");
+		}
 		
+		JSONObject json = null;
+		try{
+			json = JSON.parseObject(postData);
+			if(json==null){
+				return errorPayInfo("客户端数据解析异常");
+			}
+		}catch(Exception e){
+			return errorPayInfo("客户端数据解析异常");
+		}
+		
+		
+		String channel = json.getString("channel");
+		if(StringUtils.isBlank(channel)){
+			return errorPayInfo("支付渠道为空");
+		}
+		
+		String amount = json.getString("amount");
+		if(StringUtils.isBlank(amount)){
+			return errorPayInfo("支付金额为空");
+		}
+		
+		String spId = json.getString("sp_id");
+		if(StringUtils.isBlank(spId)){
+			return errorPayInfo("商家编号为空");
+		}
+		
+		Charge charge = chargeService.shipperRecharge(channel, amount, spId);
+		
+		return charge;
+	}
+	
+	@RequestMapping("/pay_success")
+	public @ResponseBody Object paySuccessHooks(HttpServletRequest req){
+		
+		try{
+			getPostData(req);
+		}catch(Exception e){
+			
+		}
 		
 		return null;
 	}
 	
+	private String getPostData(HttpServletRequest req) {
+		StringBuffer sb = new StringBuffer("");
+		try {
+			InputStreamReader in = new InputStreamReader(req.getInputStream());
+			BufferedReader bufReader = new BufferedReader(in);
+
+			String buf = bufReader.readLine();
+			while (buf != null) {
+				sb.append(buf);
+				buf = bufReader.readLine();
+			}
+		} catch (Exception e) {
+			throw new BaseSupportException(e);
+		}
+		
+		System.out.println(sb);
+		return sb.toString();
+	}
+	
+	/**
+	 * 返回操作对象默认值(操作出现异常)
+	 * @param errorInfo
+	 * @return
+	 */
+	public ResultEntity getErrorEntity(String errorInfo){
+		if(StringUtils.isNotBlank(errorInfo)){
+			errorInfo = errorInfo.replace("java.lang.reflect.UndeclaredThrowableException", "system error");
+			errorInfo = errorInfo.replace("com.ksb.openapi.error.BaseSupportException", "");
+		}
+		ResultEntity rs = new ResultEntity();
+		rs.setSuccess(false);
+		rs.setErrors(errorInfo);
+		rs.setObj("");
+		return rs;
+	}
 	
 	
+	public Map<String, String> errorPayInfo(String msg){
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("ksb_result", "false");
+		map.put("ksb_msg", msg);
+		return map;
+	}
+	
+	/**
+	 * 返回操作对象默认值(操作正常)
+	 * @param obj
+	 * @return
+	 */
+	public ResultEntity getSuccessEntity(Object obj){
+		ResultEntity rs = new ResultEntity();
+		rs.setSuccess(true);
+		rs.setErrors("OK");
+		rs.setObj(obj);
+		return rs;
+	} 
 }
